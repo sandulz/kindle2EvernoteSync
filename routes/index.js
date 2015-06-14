03f5159958a6ea33c3f18cd51bd2280d95609e92
Alexander Close <alexander.close@gmail.com>
2015-06-13 21:26:37 -0700

    Split Clipping Data

--- routes/index.js
+++ routes/index.js
@@ -87,33 +87,43 @@ exports.clear = function(req, res) {
 
 // Fetch Local File and Push to Evernote Web
 function readFile(file, callback) {
-    fs.readFile(file, 'utf8', function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        console.log('OK: ' + file);
-        callback(data);
-    });
+  fs.readFile(file, 'utf8', function(err, data) {
+      if (err) {
+          console.log(err);
+      }
+      console.log('OK: ' + file);
+      callback(data);
+  });
 }
-var output = readFile('/Users/alexander.close2/Dropbox/books/kindleClippings/newestKindleClippings.txt', function(data) {
-    dataSplit = data.split('==========');
-
-    var title = dataSplit[0].split('\r\n- ')[0].replace('\r\n',''),
-        date = dataSplit[1].split('\r\n')[0],
-        location = dataSplit[0].split('\r\n- ')[1].replace('\r\n',''),
-        text = dataSplit[1].split('\r\n\r\n')[1];
-
-
+var output = readFile('/Users/alexander.close2/Dropbox/books/kindleClippings/sandboxaddToEvernoteClippings.txt', function(data) {
+var dataSplit = data.split('==========');
+  for (var i = 0; i < dataSplit.length - 1; i++) {
+    var title = dataSplit[i].split('\r\n- ')[0].replace('\r\n','');
+    var text = dataSplit[i].split('\r\n\r\n')[1];
 
     console.log(title);
-    console.log(date);
-    console.log(location);
     console.log(text);
+
+  }
 
 
 });
 
 
 
+
+/*
+==========
+Legacy (Kerr, James)
+- Your Highlight on page 3 | location 120-121 | Added on Saturday, 7 February 2015 17:59:51
+
+Successful leaders balance pride with humility: absolute pride in performance; total humility before the magnitude of the task.
+==========
+*/
+/*
+[ 'The Hard Thing About Hard Things: Building a Business When There Are No Easy Answers (Horowitz, Ben)\r\n- Your Highlight on page 172 | location 2574-2575 | Added on Saturday, 23 August 2014 19:17:04\r\n\r\n“Do I value internal or external knowledge more for this position?” will help you determine whether to go for experience or youth.\r\n',
+  '\r\nZen and the Art of Motorcycle Maintenance: An Inquiry Into Values (Robert M. Pirsig)\r\n- Your Highlight at location 1598-1599 | Added on Wednesday, 17 September 2014 22:30:15\r\n\r\ngreatest part of his work is careful observation and precise thinking.\r\n',
+  '' ]
+*/
 
 
